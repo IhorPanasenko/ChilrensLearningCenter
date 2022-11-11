fb00c1163f0e6c71a5e0ef4a7029ef026cfa85e5
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 19:44:02 +0300

    Create SpecialistRepository.cs

---
+++ ChildrensLearningCenterWeb/DAL/Repositories/SpecialistRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class SpecialistRepository : ISpecialistsRepository
+    {
+        private readonly ChildrensLearningCenterContext _dbContext;
+
+        public SpecialistRepository(ChildrensLearningCenterContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Specialist> GetAll()
+        {
+            return _dbContext.Specialists.ToList();
+        }
+    }
+}
