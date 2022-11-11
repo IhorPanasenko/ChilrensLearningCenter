dacb876bb8aa5a39f66f1e832d27a6dd7ec6539e
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 14:33:12 +0300

    Create IChildrenService interface

---
+++ ChildrensLearningCenterWeb/BLL/Interfaces/IChildrenService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    internal interface IChildrenService
+    {
+        List<Children> GetAll();
+        Children? Get(int id);
+        void Create(Children children);
+        void Update(int id, Children children);
+        void Delete(int id);
+    }
+}
