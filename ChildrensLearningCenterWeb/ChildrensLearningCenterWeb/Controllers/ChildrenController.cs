2bfb0e92a9f19bb6ea099060a8ac0fcec9a53101
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 15:09:51 +0300

    implement Get in ChildrenController

---
+++ ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/Controllers/ChildrenController.cs
@@ -0,0 +1,72 @@
+﻿using BLL.Interfaces;
+using ChildrensLearningCenterWeb.ViewModels;
+using Core.Models;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ChildrensLearningCenterWeb.Controllers
+{
+    public class ChildrenController: ControllerBase
+    {
+        private readonly IChildrenService childrenService;
+
+        private readonly ILogger<ChildrenController> logger;
+        public ChildrenController(IChildrenService childrenService, ILogger<ChildrenController> logger)
+        {
+            this.childrenService = childrenService;
+            this.logger = logger;
+        }
+
+        [HttpGet("{id=int}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            try
+            {
+                Children? children = childrenService.Get(id); ;
+
+                if(children == null)
+                {
+                    return NotFound($"No child with id {id}");
+                }
+
+                var childrenViewModel = getChildrenViewModel(children);
+                return Ok(childrenViewModel);
+
+            }
+            catch (ArgumentNullException e)
+            {
+                logger.LogError(e.Message);
+                return ValidationProblem(e.Message);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return ValidationProblem(e.Message);
+            }
+        }
+
+        private ChildrenViewModel? getChildrenViewModel (Children children)
+        {
+            try
+            {
+                //var client = 
+                return new ChildrenViewModel
+                {
+                    ChildId = children.ChildId,
+                    FirstName = children.FirstName,
+                    SecondName = children.SecondName,
+                    BirthdayDate = children.BirthdayDate,
+                    ClientId = children.ClientId
+                };
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return null;
+        }
+
+    }
+}
