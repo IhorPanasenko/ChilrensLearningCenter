512d91971f326874bd9578fc0bf30c9e6ea326fd
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 01:42:13 +0300

    Create DirectionRepository.cs

---
+++ ChildrensLearningCenterWeb/DAL/Repositories/DirectionRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class DirectionRepository : IDirectionRepository
+    {
+        private readonly ChildrensLearningCenterContext _dbContext;
+
+        public DirectionRepository(ChildrensLearningCenterContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Direction> GetAll()
+        {
+            return _dbContext.Directions.ToList();
+        }
+
+        public void StoredProcedure(int price)
+        {
+            _dbContext.Database.ExecuteSqlRaw($"Add_Description {price}");
+            _dbContext.SaveChanges();
+        }
+    }
+}
