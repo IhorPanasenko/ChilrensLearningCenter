d0eb7c5c2fdb42fdfdf912db8446a9868092720f
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 01:42:17 +0300

    Create IDirectionService.cs

---
+++ ChildrensLearningCenterWeb/BLL/Interfaces/IDirectionService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IDirectionService
+    {
+        List<Direction> GetAll();
+
+        void StoredPeocedure(int price);
+    }
+}
