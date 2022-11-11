ede04564c1408687b5469aa35d71eb1213402a47
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 02:01:03 +0300

    Create StoredFunctionsController.cs

---
+++ ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/Controllers/StoredFunctionsController.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildrensLearningCenterWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoredFunctionsController : ControllerBase
+    {
+        private readonly ChildrensLearningCenterContext _dbContext;
+
+        public StoredFunctionsController(ChildrensLearningCenterContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult ScalarFunction()
+        {
+            //var res = _dbContext.Database.SqlQuery<int>("SELECT [dbo].[ManyOldestSpecialistsDirection] ()");
+            int res = _dbContext.Database.
+            return Ok(res);
+        }
+    }
+}
