e19a158223ab73c53b183a5afb46606c86b3c982
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 19:42:01 +0300

    Create ClassViewModel.cs

---
+++ ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/ViewModels/ClassViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ChildrensLearningCenterWeb.ViewModels
+{
+    public class ClassViewModel
+    {
+        public int ClassId { get; set; }
+        public string DayOfWeek { get; set; } = null!;
+        public TimeSpan Time { get; set; }
+        public int SpecialistId { get; set; }
+        public int ChildId { get; set; }
+    }
+}
