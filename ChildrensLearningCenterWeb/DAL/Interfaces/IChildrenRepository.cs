2de6a280f8473e9f3543006fba051529b3da0ead
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 14:10:25 +0300

    Create IChildrenRepository interface

---
+++ ChildrensLearningCenterWeb/DAL/Interfaces/IChildrenRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    internal interface IChildrenRepository
+    {
+        List<Children> GetAll();
+        Children Get(int id);
+        void Create(Children children);
+        void Update(int id, Children children);
+        void Delete (int id);
+    }
+}
