cfa1ed4d3d8277317ab1ebfb82b344e4031d7f4e
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 19:22:56 +0300

    Create ClassRepository.cs

---
+++ ChildrensLearningCenterWeb/DAL/Repositories/ClassRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    internal class ClassRepository : IClassRepository
+    {
+        private readonly ChildrensLearningCenterContext _dbContext;
+
+        public ClassRepository(ChildrensLearningCenterContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Class> GetAll()
+        {
+            var classes = _dbContext.Classes.ToList();
+            return classes;
+        }
+    }
+}
