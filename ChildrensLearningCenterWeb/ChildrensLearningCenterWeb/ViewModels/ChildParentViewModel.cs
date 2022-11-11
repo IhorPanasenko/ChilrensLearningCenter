4ccad71136590e8cd7d7d696d202aa054d8426f7
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 01:24:33 +0300

    Create ChildParentViewModel.cs

---
+++ ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/ViewModels/ChildParentViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ChildrensLearningCenterWeb.ViewModels
+{
+    public class ChildParentViewModel
+    {
+        public string ClientFirstName { get; set; }
+        public string ClientSecondName { get; set; }
+
+        public string ClientTelephone { get; set; }
+
+        public string ClientEmail { get; set; }
+
+        public DateTime ClientBirthdayDate { get; set; }
+
+        public string ChildFirstName { get; set; }
+        public string ChildSecondName { get; set; }
+        public DateTime ChildBirthdayDate { get; set; }
+    }
+}
