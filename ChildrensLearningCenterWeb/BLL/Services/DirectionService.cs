d6e8fd8e86dfb6066ca7f2dec8412471c5368832
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 01:42:21 +0300

    Create DirectionService.cs

---
+++ ChildrensLearningCenterWeb/BLL/Services/DirectionService.cs
@@ -0,0 +1,52 @@
+﻿using BLL.Interfaces;
+using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class DirectionService : IDirectionService
+    {
+        private readonly IDirectionRepository directionRepository;
+
+        private readonly ILogger<DirectionService> logger;
+
+        public DirectionService(IDirectionRepository directionRepository, ILogger<DirectionService> logger)
+        {
+            this.directionRepository = directionRepository;
+            this.logger = logger;
+        }
+
+        public List<Direction> GetAll()
+        {
+            List<Direction> directions = new List<Direction>();
+            try
+            {
+                directions = directionRepository.GetAll();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return directions;
+        }
+
+        public void StoredPeocedure(int price)
+        {
+            try
+            {
+                directionRepository.StoredProcedure(price);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+        }
+    }
+}
