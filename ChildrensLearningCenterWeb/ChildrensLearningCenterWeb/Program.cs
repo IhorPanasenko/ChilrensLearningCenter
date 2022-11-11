1b016814cab36e7770f059276b623b9ddaacb1af
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 19:20:52 +0300

    Add Services in program.cs

--- ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/Program.cs
+++ ChildrensLearningCenterWeb/ChildrensLearningCenterWeb/Program.cs
@@ -1,3 +1,9 @@
+using BLL.Interfaces;
+using BLL.Services;
+using DAL;
+using DAL.Interfaces;
+using DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IChildrenRepository, ChildrenRepository>();
+builder.Services.AddScoped<IChildrenService, ChildrenService>();
+
+builder.Services.AddDbContext<ChildrensLearningCenterContext>();
 
 var app = builder.Build();
 
