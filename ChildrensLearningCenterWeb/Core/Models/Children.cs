7b03b9552698f5ba54cbe6fde286d98f61ea0375
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-11 14:04:00 +0300

    Creating models and DbContext
    By using Entity framework, database first using Scaffold-DbContext

---
+++ ChildrensLearningCenterWeb/Core/Models/Children.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public partial class Children
+    {
+        public Children()
+        {
+            Classes = new HashSet<Class>();
+        }
+
+        public int ChildId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? SecondName { get; set; }
+        public DateTime BirthdayDate { get; set; }
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; } = null!;
+        public virtual ICollection<Class> Classes { get; set; }
+    }
+}
---
+++ ChildrensLearningCenterWeb/Core/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Models
+{
+    public partial class Client
+    {
+        public Client()
+        {
+            Children = new HashSet<Children>();
+        }
+
+        public int ClientId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? SecondName { get; set; }
+        public string TelephoneNumber { get; set; } = null!;
+        public string? Email { get; set; }
+        public DateTime? BirthdayDate { get; set; }
+
+        public virtual ICollection<Children> Children { get; set; }
+    }
+}
