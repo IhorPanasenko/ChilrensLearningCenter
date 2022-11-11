d3e3c545a54233972750f48b2821f8e986cfc29a
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 01:42:11 +0300

    Update IDirectionRepository.cs

--- ChildrensLearningCenterWeb/DAL/Interfaces/IDirectionRepository.cs
+++ ChildrensLearningCenterWeb/DAL/Interfaces/IDirectionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace DAL.Interfaces
 {
     public interface IDirectionRepository
     {
-        void GetAll();
+        List<Direction> GetAll();
+
+        void StoredProcedure(int price);
     }
 }
