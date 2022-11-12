cc87ed96f6ece65ef94a75545f0813a556e8add1
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-12 14:16:02 +0300

    Create StoredFunctionTableModel.cs

---
+++ ChildrensLearningCenterWeb/Core/Models/StoredFunctionTableModel.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildrensLearningCenterWeb.ViewModels
+{
+    [Keyless]
+    public class StoredFunctionTableModel
+    {
+        public StoredFunctionTableModel()
+        {
+            Classes = new HashSet<Class>();
+        }
+        public int SpecialistID { get; set; }
+        public int DirectionID { get; set; }
+
+        public virtual ICollection<Class> Classes { get; set; }
+
+    }
+}
